7c314bc9f572e50bb6ed262c67fbaaed0b2680f1
Henry AT <yiperu@gmail.com>
2013-09-27 07:29:01 -0500

    Inicializacion del proyecto, ademas de referenciar los framework CoreLocation y UIKit

---
+++ miLocalizacion_v01/miLocalizacion_v01/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  miLocalizacion_v01
+//
+//  Created by Henry AT on 9/27/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
